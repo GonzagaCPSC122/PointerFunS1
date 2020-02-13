0d267967cfec9a8e8fc981134fd6d5c62c8bf1c6
Gina Sprint <sprint@gonzaga.edu>
2020-02-13 15:30:37 -0800

    finish cmd line args

--- CmdLineArgsFun.cpp
+++ CmdLineArgsFun.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
-// comnand line args are passed into the program when it is run
+// command line args are passed into the program when it is run
 // main() is overloaded to accepts these arguments
 int main(int argc, char * argv[]) {
 	// C-string is a null terminated char array
@@ -14,6 +15,19 @@ int main(int argc, char * argv[]) {
 		string arg = argv[i]; // assign C-string to C++ string
 		cout << arg << endl; // print out C++ string
 	}
+	// 0 1 2 3 4
+	// f o o d \0
+	// #include <cstring> you can use several
+	// C-string functions
+	// strlen()
+	char myWord[] = "food";
+	cout << myWord << endl;
+	cout << strlen(myWord) << endl;
+	cout << sizeof(myWord) << endl; // 5 chars * 1 byte per char = 5 bytes
+	// other useful cstring functions
+	// strcat() +
+	// strcmp() < > <=...
+	// strcpy() copy
 
 	return 0;
 }
